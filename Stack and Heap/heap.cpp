8a65fe1e291e2bec5883a3d919f07bd4c08f224d
Rajdeep Sarkar <76910906+RajdeepSarkar2000@users.noreply.github.com>
2021-10-06 10:05:06 +0530

    Add files via upload

---
+++ Stack and Heap/heap.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=10; //stored in stack
+int*p=new int();//allocate memory in heap
+*p=10;
+delete(p);//deallocate memory
+p=NULL;//updates memory as null
+return 0 ;
+}
